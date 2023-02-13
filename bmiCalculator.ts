748df2ea7805feb3751c9058e2a63bd1c0449be7
Amir thapa magar <amirthapa2019@gmail.com>
2023-02-13 17:07:03 +0545

    feat:finish 9.1

---
+++ bmiCalculator.ts
@@ -0,0 +1,15 @@
+const calculateBmi = (height: number, weight: number): String => {
+  const bmiHeightInSquareMeter = (height / 100) * (height / 100);
+  const bmi = weight / bmiHeightInSquareMeter;
+  if (bmi < 18.5) {
+    return "Not Normal (Underweight)";
+  } else if (bmi > 18.5 && bmi < 25.9) {
+    return "Normal (Healthy Weight)";
+  } else if (bmi > 25.0 && bmi < 29.9) {
+    return "Not Normal (Overweight)";
+  } else {
+    return "Not Normal (Obesity)";
+  }
+};
+
+console.log(calculateBmi(180, 74));
